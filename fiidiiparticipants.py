3b3e8d83a01388c69167d5396fe58563b7f67cf9
tsrajpurohit <156318103+tsrajpurohit@users.noreply.github.com>
2025-09-15 21:01:08 +0530

    Update fiidiiparticipants.py

--- fiidiiparticipants.py
+++ fiidiiparticipants.py
@@ -28,6 +28,9 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0",
     "Referer": "https://www.nseindia.com/",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/120.0.0.0 Safari/537.36"
 }
 
 # Function to download and parse CSV
