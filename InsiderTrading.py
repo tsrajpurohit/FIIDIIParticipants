22ae764fbb5b49ae2f0e3843c9ab0c9b1d99308e
tsrajpurohit <156318103+tsrajpurohit@users.noreply.github.com>
2026-06-30 21:36:16 +0530

    Update InsiderTrading.py

--- InsiderTrading.py
+++ InsiderTrading.py
@@ -19,9 +19,12 @@ print(f"Generated Dynamic URL: {API_URL}")
 
 # Standard headers to simulate a browser session
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    "Accept": "application/json,text/plain,*/*",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.nseindia.com/",
+    "Origin": "https://www.nseindia.com",
+    "Connection": "keep-alive",
 }
 
 
